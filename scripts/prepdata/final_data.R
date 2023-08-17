20a83cbf57cf93e8a7ad588cc50b954b7eef3d20
rubysaltbush <stephenseruby@gmail.com>
2023-08-17 18:00:35 +1000

    scoring symmetry more and more, estimate 7 days left at this pace maybe?

--- scripts/prepdata/final_data.R
+++ scripts/prepdata/final_data.R
@@ -146,7 +146,7 @@ symscored <- symscored %>%
   dplyr::filter(!is.na(Accepted_name)) %>%
   dplyr::distinct()
 sum(is.na(symscored$sym_species))
-# 637 taxa left to score symmetry for
+# 592 taxa left to score symmetry for
 
 sym_long <- sym_long %>%
   dplyr::select(-sym_species) %>%
