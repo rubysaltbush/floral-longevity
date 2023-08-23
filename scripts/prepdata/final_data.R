9d2e217afe7fde5c67037916ec0bb4fe610eb4d1
rubysaltbush <stephenseruby@gmail.com>
2023-08-23 17:19:19 +1000

    more data, up to final bit, 50 or so to go

--- scripts/prepdata/final_data.R
+++ scripts/prepdata/final_data.R
@@ -146,7 +146,7 @@ symscored <- symscored %>%
   dplyr::filter(!is.na(Accepted_name)) %>%
   dplyr::distinct()
 sum(is.na(symscored$sym_species))
-# 230 taxa left to score symmetry for
+# 64 taxa left to score symmetry for
 
 sym_long <- sym_long %>%
   dplyr::select(-sym_species) %>%
